d911409df3999bfbfdc8b3d3c6b94b46c6faf101
mourtashi <109982496+mourtashi@users.noreply.github.com>
2023-09-05 15:06:46 -0400

    Support for multiple webpages for the user

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-// Initialize current bid
-let currentBid = 100;
-
-// Initialize timer
+// Variables
+let currentBid = 0;
 let remainingMinutes = 9;
 let remainingSeconds = 59;
+let timer;
+
+function showProduct(name, initialBid) {
+  // Display selected product information
+  document.getElementById('item-name').innerText = name;
+  document.getElementById('current-bid').innerText = initialBid;
+  document.getElementById('new-bid').min = initialBid + 1;
+  currentBid = initialBid;
+
+  // Hide product list and show auction details
+  document.getElementById('product-list').classList.add('hidden');
+  document.getElementById('auction-item').classList.remove('hidden');
+  document.getElementById('timer').classList.remove('hidden');
+  
+  // Start the timer
+  if (timer) {
+    clearInterval(timer);
+  }
+  timer = setInterval(updateTimer, 1000);
+}
+
+function hideProduct() {
+  // Hide auction details and show product list
+  document.getElementById('auction-item').classList.add('hidden');
+  document.getElementById('timer').classList.add('hidden');
+  document.getElementById('product-list').classList.remove('hidden');
+  
+  // Stop the timer
+  clearInterval(timer);
+}
 
 function placeBid() {
   const newBid = parseInt(document.getElementById('new-bid').value);
   if (newBid > currentBid) {
     currentBid = newBid;
     document.getElementById('current-bid').innerText = currentBid;
+    document.getElementById('new-bid').min = currentBid + 1;
   } else {
     alert('Your bid must be higher than the current bid.');
   }
@@ -25,6 +54,7 @@ function updateTimer() {
     if (remainingMinutes === 0) {
       clearInterval(timer);
       alert('Auction ended.');
+      hideProduct();
       return;
     } else {
       remainingMinutes--;
@@ -34,6 +64,3 @@ function updateTimer() {
     remainingSeconds--;
   }
 }
-
-// Start the timer
-const timer = setInterval(updateTimer, 1000);
